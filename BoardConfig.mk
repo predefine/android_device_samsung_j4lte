d81b888c55566768442c1b476cd8a1e785382c7a
redline-dev <73928476+redline-dev@users.noreply.github.com>
2020-11-22 05:27:26 +0700

    Try to fix 2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -44,12 +44,13 @@ TARGET_KERNEL_SOURCE := kernel/samsung/j4lte
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 
 # Image
-BOARD_CUSTOM_BOOTIMG_MK := device/samsung/j4ltejx/mkbootimg.mk
+#BOARD_CUSTOM_BOOTIMG_MK := device/samsung/j4ltejx/mkbootimg.mk
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --dt $(OUT_DIR)/target/product/j4ltejx/obj/KERNEL_OBJ/arch/$(TARGET_ARCH)/boot/dtb.img
+
 #TARGET_CUSTOM_DTBTOOL := dtbhtoolExynos
 
 # File systems
