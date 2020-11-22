78efe0b45f27f0a5bccca52547d0d39285bf6144
redline-dev <73928476+redline-dev@users.noreply.github.com>
2020-11-22 07:36:42 +0700

    Adjust & Change kernel source

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -38,20 +38,17 @@ TARGET_2ND_CPU_VARIANT := cortex-a53
 
 # Kernel
 TARGET_KERNEL_ARCH := arm64
-TARGET_KERNEL_CONFIG := j4lte_00_defconfig
+TARGET_KERNEL_CONFIG := redline_defconfig
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/samsung/j4ltejx
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 TARGET_TOOLCHAIN := prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/
 
 # Image
-#BOARD_CUSTOM_BOOTIMG_MK := device/samsung/j4ltejx/mkbootimg.mk
 BOARD_KERNEL_IMAGE_NAME := Image dtb.img
-#BOARD_KERNEL_SEPARATED_DT := true
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --dt $(OUT_DIR)/target/product/j4ltejx/obj/KERNEL_OBJ/arch/$(TARGET_ARCH)/boot/dtb.img
-#TARGET_CUSTOM_DTBTOOL := dtbhtoolExynos
 
 # File systems
 BOARD_HAS_LARGE_FILESYSTEM := true
