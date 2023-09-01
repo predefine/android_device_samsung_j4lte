c7b8d3c7d49679c8de5f9f2e0467db76dea63f77
predefine <predefine@yandex.ru>
2023-09-02 02:23:27 +0700

    first commit

--- omni_j4lte.mk
+++ omni_j4lte.mk
@@ -1,8 +1,16 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 $(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 PRODUCT_DEVICE := j4lte
 PRODUCT_NAME := omni_j4lte
 PRODUCT_MODEL := SM-J400
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
+
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_RETROFIT_DYNAMIC_PARTITIONS := true
+
+# Fastbootd ( broken )
+#PRODUCT_PACKAGES += fastbootd
